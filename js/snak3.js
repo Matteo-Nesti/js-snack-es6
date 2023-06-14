3478ad7745f376ec000e5d28e41c585cdcee4e5c
Matteo-Nesti <matteo.nesti@hotmail.com>
2023-06-14 15:14:18 +0200

    start snak 3

---
+++ js/snak3.js
@@ -0,0 +1,28 @@
+/* 
+Creare un array di oggetti:
+Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: name e weight.
+Stampare in console la bici con peso minore utilizzando il destructuring
+*/
+
+const bikes = [
+  {
+    name: "bici 1",
+    wieght: 25,
+  },
+  {
+    name: "bici 2",
+    wieght: 30,
+  },
+  {
+    name: "bici 3",
+    wieght: 20,
+  },
+  {
+    name: "bici 4",
+    wieght: 35,
+  },
+  {
+    name: "bici 5",
+    wieght: 10,
+  },
+];
